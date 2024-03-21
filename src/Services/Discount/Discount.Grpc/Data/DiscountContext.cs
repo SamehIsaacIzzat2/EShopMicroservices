8a799805dfac75c8d87c52b24910d39db8bfca23
License 48 <license48@flairstech.com>
2024-03-21 22:35:39 +0200

    WIP

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -3,13 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Data
 {
-    public class DiscountContext :DbContext
+    public class DiscountContext : DbContext
     {
         public DbSet<Coupon> Coupons { get; set; } = default!;
 
         public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon() { Id = 1, Description = "Item 1 des", ProductName = "Iphone X", Amount = 2 },
+                new Coupon() { Id = 2, Description = "Item 2 des", ProductName = "Iphone 15", Amount = 2 });
         }
     }
 }
