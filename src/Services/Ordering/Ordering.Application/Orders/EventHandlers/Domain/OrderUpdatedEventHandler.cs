9cca8a839699f52c5e5554faa772883b3bf352e7
License 48 <license48@flairstech.com>
2024-03-27 14:36:33 +0200

    Ordering.Application and Ordering.API

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Orders.EventHandlers.Domain
+{
+    internal class OrderUpdatedEventHandler : INotificationHandler<OrderUpdatedEvent>
+    {
+        public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
